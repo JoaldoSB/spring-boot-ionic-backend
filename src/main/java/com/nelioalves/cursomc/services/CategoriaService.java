ebd883ca1f161e146d5f3ab4ea5d7439ca4d59c2
Joaldo Santos <joaldo.einstein@hotmail.com>
2020-10-20 08:10:07 -0300

    Ajustes no endpoint /categorias/{id} v2

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -15,16 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Optional<Categoria> buscar(Integer id) {
-		
-		Optional<Categoria> obj = repo.findById(id);
-		if (obj == null) {
-			
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " +id
-					+" , Tipo: " +Categoria.class.getName());
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj= repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+ id+ ", Tipo: "+ Categoria.class.getName()));
 		}
-		return obj;
-		
-	}
-
 }
